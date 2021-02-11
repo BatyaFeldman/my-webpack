77f3aa759b400694e614baedd8c088b1f78ddcc7
BatyaFeldman <batyashaps1@gmail.com>
2021-02-10 20:46:07 -0600

    using html plugins to deal with cache busting

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
+    //add content hash to prevent browser from caching the main.js. the hash will change everytime something in main.js changes
+    filename: "main.[contenthash].js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
   module: {
     rules: [
       {
